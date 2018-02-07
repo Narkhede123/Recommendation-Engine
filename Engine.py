15f8a5b4c0816fe28f8280f488e7ccdc84fb5770
Narkhede123 <32284984+Narkhede123@users.noreply.github.com>
2018-02-07 16:08:53 +0530

    Update Engine.py

--- Engine.py
+++ Engine.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-
-
+ 
+#load data 
 header = ['user_id', 'item_id', 'rating', 'timestamp']
-df = pd.read_csv('ml-100k/u.data', sep='\t', names=header)
+df = pd.read_csv('mvlense.data', sep='\t', names=header)
 n_users = df.user_id.unique().shape[0]
 n_items = df.item_id.unique().shape[0]
 print 'Number of users = ' + str(n_users) + ' | Number of movies = ' + str(n_items)
